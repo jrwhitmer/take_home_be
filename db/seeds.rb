0dc71d3e705ead8a2b903ba69f9773a243f61223
Jacq Whitmer <jacqwhitmer@gmail.com>
2022-01-12 07:23:39 -0800

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,13 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+@customer_1 = Customer.create!(first_name: "First", last_name: "Last", email: "email@email.com", address: "123 Address St")
+@customer_2 = Customer.create!(first_name: "First 2", last_name: "Last 2", email: "email2@email.com", address: "234 Address St")
+@subscription_1 = Subscription.create!(title: "Subscription 1", price: "11.99", status: "active", frequency: "monthly")
+@subscription_2 = Subscription.create!(title: "Subscription 2", price: "12.99", status: "cancelled", frequency: "monthly")
+@subscription_3 = Subscription.create!(title: "Subscription 3", price: "13.99", status: "active", frequency: "monthly")
+@customer_subscription_1 = CustomerSubscription.create!(customer_id: @customer_1.id, subscription_id: @subscription_1.id)
+@customer_subscription_2 = CustomerSubscription.create!(customer_id: @customer_1.id, subscription_id: @subscription_2.id)
+@customer_subscription_3 = CustomerSubscription.create!(customer_id: @customer_2.id, subscription_id: @subscription_3.id)
